3c5bc660ca1051d91188e8151f0a33dba2c9a9e6
Fraol-X <pfresh641@gmail.com>
2024-12-16 10:05:21 +0300

    Using components

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,10 +1,15 @@
-const Pill: React.FC = () => {
-    return (
-      <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
-        <p className=" text-sm ">Title</p>
-      </div>
-    )
-  }
-  
-  export default Pill;
-  
+import React from 'react';
+
+interface PillProps {
+  title: string; 
+}
+
+const Pill: React.FC<PillProps> = ({ title }) => {
+  return (
+    <div className="flex justify-center items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+      <p className="text-sm">{title}</p>
+    </div>
+  );
+};
+
+export default Pill;
